32420080370908846b6afe1f5a694141c3f0e7cc
Roman Birg <romanbirg@gmail.com>
2012-08-05 16:44:35 -0500

    ROMControl: Add weather settings back
    p2: cleaned up xmls
    p3: added sound fragment back
        cleaned up angry red boxes
    p4: rebase
    p5: fix roman's HERPITY DERP DERP
    
    Change-Id: Ib7df8268ad99253606df65899e82d5238a1ceb31

--- src/com/aokp/romcontrol/service/BootService.java
+++ src/com/aokp/romcontrol/service/BootService.java
@@ -12,6 +12,9 @@ import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 
+import com.aokp.romcontrol.weather.WeatherRefreshService;
+import com.aokp.romcontrol.weather.WeatherService;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -56,6 +59,12 @@ public class BootService extends Service {
                     || FlipService.getUserCallSilent(c) != 0)
                 c.startService(new Intent(c, FlipService.class));
 
+            if (Settings.System
+                    .getBoolean(getContentResolver(), Settings.System.USE_WEATHER, false)) {
+                sendLastWeatherBroadcast();
+                getApplicationContext().startService(new Intent(c, WeatherRefreshService.class));
+            }
+
             return null;
         }
 
@@ -65,6 +74,32 @@ public class BootService extends Service {
             servicesStarted = true;
             stopSelf();
         }
+    }
+
+    private void sendLastWeatherBroadcast() {
+        SharedPreferences settings =
+                getApplicationContext().getSharedPreferences(WeatherService.PREFS_NAME, 0);
+
+        Intent broadcast = new Intent(WeatherService.INTENT_WEATHER_UPDATE);
+        try {
+            broadcast.putExtra(WeatherService.EXTRA_CITY, settings.getString("city", ""));
+            broadcast.putExtra(WeatherService.EXTRA_CONDITION, settings.getString("condition", ""));
+            broadcast.putExtra(WeatherService.EXTRA_LAST_UPDATE,
+                    settings.getString("timestamp", ""));
+            broadcast.putExtra(WeatherService.EXTRA_CONDITION_CODE,
+                    settings.getString("condition_code", ""));
+            broadcast.putExtra(WeatherService.EXTRA_FORECAST_DATE,
+                    settings.getString("forecast_date", ""));
+            broadcast.putExtra(WeatherService.EXTRA_HUMIDITY, settings.getString("humidity", ""));
+            broadcast.putExtra(WeatherService.EXTRA_TEMP, settings.getString("temp", ""));
+            broadcast.putExtra(WeatherService.EXTRA_WIND, settings.getString("wind", ""));
+            broadcast.putExtra(WeatherService.EXTRA_LOW, settings.getString("low", ""));
+            broadcast.putExtra(WeatherService.EXTRA_HIGH, settings.getString("high", ""));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        getApplicationContext().sendBroadcast(broadcast);
     }
 
     @Override
